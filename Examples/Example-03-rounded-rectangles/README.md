6e14ac897bf236a833fbba197a57552eb3400cca
sillensimmar <47142115+sillensimmar@users.noreply.github.com>
2019-02-20 21:04:19 +0100

    Delete old files

--- Examples/Example-03-rounded-rectangles/README.md
+++ Examples/Example-03-rounded-rectangles/README.md
@@ -1,5 +1,3 @@
 This project is called Rounded Rectangles and is edited by Klara.
 
-Rounded Rectangles consists of a seven layered square, every square has its own color. Each time the user clicks their mouse seven more layers are added. When the user moves their mouse the stack is displayed from the side, following the movement of the mouse. The user also interacts with the project by changing the speed, the degree of rotation and the border radius of the squares. STAR?
-
-The project is a fun representation of movement and color, being able to see the stack of squares from different angles.
+Rounded Rectangles is a fun representation of shapes, movement and color. It is based on a seven layered object, every layer has its own color. Each time the user clicks their mouse seven more layers are added to the stack. When the user moves their mouse the stack follows its movements. The user also interacts with the object by changing speed, degree of rotation, border radius of the square and the points of the star, using the sliders/checkbox.
--- Examples/Example-03-rounded-rectangles/main.js
+++ Examples/Example-03-rounded-rectangles/main.js
@@ -1,23 +1,46 @@
 //Where stack starts on canvas 
 var mousePoint = view.center;
-//Amount of layers
+	
+//Variables for sliders
 var amount = 7;
+var twistNumber = 10;
+var speedNumber = 5;
+var roundnessNumber = 5;
+
+//Shape boolean
+var isStar = false;
+
 //Colors of layers
 var colors = ['#5942f4', '#a041f4', '#d63ff4', '#f23ad6', 'orange', 'white', 'green']
 
-//First loop
-for (var i = 0; i < amount; i++) {
-	//Measurements on rectangle
-	var rect = new Rectangle([0, 0], [25, 25]);
-	rect.center = mousePoint;
-	//Roundness of square-corners
-	var path = new Path.Rectangle(rect, 0);
-	//Amount of colors
-	path.fillColor = colors[i % 7];
-	//Scale on canvas
-	var scale = (1 - i / amount) * 20;
-	path.scale(scale);
+//Looping draw function
+function createShapes() {
+    project.activeLayer.children = [];
+	for (var i = 0; i < amount; i++) {
+		//Conditional checkbox shape
+		if (isStar === true) {
+			//Measurements of star
+			var path = new Path.Star(mousePoint, roundnessNumber, 15, 30);
+			//Scale on canvas
+			var scale = (1 - i / amount) * 10;
+			path.scale(scale);
+		} else {
+			//Measurements on rectangle
+			var rect = new Rectangle([0, 0], [25, 25]);
+			rect.center = mousePoint;
+			//Roundness of square-corners
+			var path = new Path.Rectangle(rect, roundnessNumber);
+			//Scale on canvas
+			var scale = (1 - i / amount) * 20;
+			path.scale(scale);
+		}
+		//Amounts of colors
+		path.fillColor = colors[i % 7];
+	}
 }
+
+//First loop
+createShapes();
 
 //The stack following the mouse
 function onMouseMove(event) {
@@ -26,21 +49,9 @@ function onMouseMove(event) {
 
 //When clicking the mouse
 function onMouseDown(event) {
-	project.activeLayer.children = [];
-	//Adding 7 layers each time the mouse is clicked>
+	//Adding 7 layers each time the mouse is clicked
 	amount = amount + 7;
-	for (var i = 0; i < amount; i++) {
-		//Measurements on rectangle
-		var rect = new Rectangle([0, 0], [25, 25]);
-		rect.center = mousePoint;
-		//Roundness of square-corners
-		var path = new Path.Rectangle(rect, 0);
-		//Amounts of colors
-		path.fillColor = colors[i % 7];
-		//Scale on canvas
-		var scale = (1 - i / amount) * 20;
-		path.scale(scale);
-	}
+	createShapes();
 	console.log(amount);
 }
 
@@ -52,9 +63,35 @@ function onFrame(event) {
 		//Movement of the square
 		var delta = (mousePoint - item.position) / (i + 5);
 		//Rotate speed
-		item.rotate(Math.sin((event.count + i) / 10) * 7);
+		item.rotate(Math.sin((event.count + i) / twistNumber) * speedNumber);
 		if (delta.length > 0.1)
 			item.position += delta;
 			
 	}
+}
+
+//Sliders/checkbox
+var speedSlider = document.getElementById("speed-number");
+var twistSlider = document.getElementById("twist-number");
+var roundnessSlider = document.getElementById("roundness-number");
+var shapeCheckbox = document.getElementById("shape-box");
+
+speedSlider.addEventListener("change", handleSliders, false);
+twistSlider.addEventListener("change", handleSliders, false);
+roundnessSlider.addEventListener("change", handleRoundnessSlider, false);
+shapeCheckbox.addEventListener("change", handleCheckbox, false);
+
+function handleSliders(event) {
+    twistNumber = twistSlider.value;
+	speedNumber = speedSlider.value;
+}
+
+function handleRoundnessSlider(event) {
+	roundnessNumber = roundnessSlider.value;
+	createShapes();
+}
+
+function handleCheckbox(event) {
+	isStar = shapeCheckbox.checked;
+	createShapes();
 }
